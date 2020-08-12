1d803001ab7912bfee2c3dbef426ba97674863f3
tRohit <tRohit@MacBook-Pro.local>
2020-08-12 10:14:06 +0530

    Initial commit

---
+++ staffManagement/staffManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  staffManagement
+//
+//  Created by tRohit on 8/12/20.
+//  Copyright © 2020 Kalsonsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
