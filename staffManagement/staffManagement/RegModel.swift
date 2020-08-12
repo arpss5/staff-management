118fae583a0eb0dee5b4c865e79831e03cc35d0a
tRohit <tRohit@MacBook-Pro.local>
2020-08-12 10:25:27 +0530

    Adding RegModel

---
+++ staffManagement/staffManagement/RegModel.swift
@@ -0,0 +1,13 @@
+//
+//  RegModel.swift
+//  staffManagement
+//
+//  Created by tRohit on 8/12/20.
+//  Copyright © 2020 Kalsonsoft. All rights reserved.
+//
+
+import UIKit
+
+class RegModel: NSObject {
+
+}
